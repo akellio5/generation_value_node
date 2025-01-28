2617e9dba93441c23e2b1d88902fece3796b53dc
unknown <kirick112044@gmail.com>
2025-01-28 02:37:55 +0200

    --

--- generation_value_node.py
+++ generation_value_node.py
@@ -1,30 +1,34 @@
-class GenerationValueNode:
-    generation_count = 0  # Счётчик сохраняется как атрибут класса
+class ImageCounterNode:
+    image_count = 0  # Глобальный счётчик обработанных изображений
 
     @classmethod
     def INPUT_TYPES(cls):
         return {
             "required": {
-                "total_generations": ("INT", {"default": 10}),  # Общее количество генераций
+                "input_image": ("IMAGE",),  # Входное изображение
+                "total_images": ("INT", {"default": 10}),  # Общее количество ожидаемых изображений
             }
         }
 
-    RETURN_TYPES = ("INT",)
-    FUNCTION = "get_value"
+    RETURN_TYPES = ("INT", "IMAGE")
+    FUNCTION = "process_image"
     CATEGORY = "Custom Nodes"
 
-    def get_value(self, total_generations):
+    def process_image(self, input_image, total_images):
+        # Увеличиваем счётчик при каждом вызове узла
+        ImageCounterNode.image_count += 1
+
         # Определение порогов для 70%, 20%, и 10%
-        threshold_70 = int(total_generations * 0.7)
-        threshold_90 = int(total_generations * 0.9)
+        threshold_70 = int(total_images * 0.7)
+        threshold_90 = int(total_images * 0.9)
 
-        # Увеличиваем счётчик текущей генерации
-        GenerationValueNode.generation_count += 1
-
-        # Определяем значение на основе текущей генерации
-        if GenerationValueNode.generation_count <= threshold_70:
-            return (100,)
-        elif GenerationValueNode.generation_count <= threshold_90:
-            return (200,)
+        # Определяем возвращаемое значение на основе счётчика
+        if ImageCounterNode.image_count <= threshold_70:
+            value = 100
+        elif ImageCounterNode.image_count <= threshold_90:
+            value = 200
         else:
-            return (300,)
+            value = 300
+
+        # Возвращаем значение и входное изображение
+        return value, input_image
