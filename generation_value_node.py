5577400c312b8a82d24fa3e0a053709241795030
unknown <kirick112044@gmail.com>
2025-01-28 02:07:58 +0200

    Исправлен баг с увеличением счётчика

--- generation_value_node.py
+++ generation_value_node.py
@@ -1,5 +1,5 @@
 class GenerationValueNode:
-    generation_count = 0  # Счетчик текущих генераций
+    generation_count = 0  # Счётчик сохраняется как атрибут класса
 
     @classmethod
     def INPUT_TYPES(cls):
@@ -14,17 +14,17 @@ class GenerationValueNode:
     CATEGORY = "Custom Nodes"
 
     def get_value(self, total_generations):
-        # Определение порогов для 70%, 20% и 10%
+        # Определение порогов для 70%, 20%, и 10%
         threshold_70 = int(total_generations * 0.7)
         threshold_90 = int(total_generations * 0.9)
 
-        # Увеличиваем счетчик текущей генерации
-        self.generation_count += 1
+        # Увеличиваем счётчик текущей генерации
+        GenerationValueNode.generation_count += 1
 
-        # Определяем значение для текущей генерации
-        if self.generation_count <= threshold_70:
+        # Определяем значение на основе текущей генерации
+        if GenerationValueNode.generation_count <= threshold_70:
             return (100,)
-        elif self.generation_count <= threshold_90:
+        elif GenerationValueNode.generation_count <= threshold_90:
             return (200,)
         else:
             return (300,)
